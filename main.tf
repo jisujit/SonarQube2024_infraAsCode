0e9a07a2154c66ba6574d48fcbf93998f9ab3bb7
Sujit Gangadharan <gsujit@gmail.com>
2024-02-19 22:36:13 -0500

    Added a few more resources rg, nrg, snet1

--- main.tf
+++ main.tf
@@ -15,14 +15,51 @@ terraform {
     }
   }
 }
-//TODO: Confirm the need for "features"
+// TODO: Confirm the need for "features"
 provider "azurerm" {
   # Configuration options
   features {}
 }
 
 //TODO: Virtual Network
-//TODO: SonarQube Subnet
-//TODO: VM
-//TODO: NIC with IP address? Difference if internal vs external?
-//TODO: VM for installing MS SQL Server
+#https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/resources/virtual_network
+// Done
+// TODO: SonarQube Subnet
+// TODO: VM
+// TODO: NIC with IP address? Difference if internal vs external?
+// TODO: VM for installing MS SQL Server
+
+resource "azurerm_resource_group" "SonarQube_rg" {
+  name     = "sonarqube-resources"
+  location = "US-East2" //SG: How can I know the list of possible values here?
+}
+
+//SG: what is this SG going to be used for? How can I give a useful name for the nsg_group-type and name?
+resource "azurerm_network_security_group" "example" {
+  name                = "example-security-group"
+  location            = azurerm_resource_group.SonarQube_rg.location
+  resource_group_name = azurerm_resource_group.SonarQube_rg.name
+}
+
+resource "azurerm_virtual_network" "example" {
+  name                = "example-network"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.0.2.0/24"
+    security_group = azurerm_network_security_group.example.id
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
